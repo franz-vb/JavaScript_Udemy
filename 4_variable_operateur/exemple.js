0820d2e3f9cb4f9f1e9e5be14a6046bc00145996
franz von büren <fz.v.bueren@gmail.com>
2021-10-12 16:00:38 +0200

    add 05

--- 4_variable_operateur/exemple.js
+++ 4_variable_operateur/exemple.js
@@ -1,1 +1,46 @@
-console.log("salut");
+//13
+// let prenom = "Marc & Simon"
+// // Chaînes de caractères (string)
+
+// let age = 21.5;
+// // Nombre (number)
+
+// let majorite = true;
+// // Booleen (boolean)
+
+// let win = false;
+// win = true;
+
+// const prenom = "Marc"; // ne peut pas être modifié
+let prenom = "Marc";
+prenom = "John";
+
+console.log(prenom);
+
+// var prenom = "Marc";
+
+//14
+const prenom  = "Nicolas";
+const nom     = "Dupont";
+
+console.log("Bonjour " + prenom + ' ' + nom + ' !');
+
+// Bonjour [prenom] [age] !
+
+//16
+// OPERATEURS
+// - "+" addition
+// - "-" soustraction
+// - "*" multiplication
+// - "/" division
+// - "%" modulo <=> 5/2 = 2.45343 restant 1
+// - "**" exponentielle 5**2 = 25
+
+let nombreUn = 8, nombreDeux = 4, result;
+result = (nombreUn + nombreDeux) * 9; // 108
+result++; // incrémentation = ajoute 1
+result--; // décrémentation = retire 1
+console.log(result);
+
+
+
---
+++ 5_informations_aux_utilisateurs/exemple.js
@@ -0,0 +1,18 @@
+//18
+alert('Hello World !');
+
+//19
+if(confirm('Souhaitez-vous confirmer ?')) {
+    // Oui
+    alert('Ok');
+  }
+  else {
+    // Non
+    alert('Annulation');
+  }
+
+//20
+const age = prompt("Entrez votre age");
+
+alert("Bonjour, vous avez " + age + " ans.");
+
