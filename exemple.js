34ba26bbcb67750a968255637b7403817e28aa87
franz von büren <fz.v.bueren@gmail.com>
2021-10-13 17:02:54 +0200

    add exo calculatrice

--- exemple.js
+++ exemple.js
@@ -1,1 +1,28 @@
-console.log("bonjour");
+do {
+    
+    var choix = Number(prompt("Que souhaitez-vous faire ?\n\n 1 - Addition\n 2 - Multiplication\n 3 - Soustraction\n 4 - Division\n"));
+
+} while(choix != 1 && choix != 2 && choix != 3 && choix != 4)
+
+do {
+
+    var premierNombre   = Number(prompt("Entrez un premier nombre"));
+    var deuxiemeNombre  = Number(prompt("Entrez un deuxième nombre"));
+
+} while (isNaN(premierNombre) || isNaN(deuxiemeNombre));
+
+nombreA = premierNombre;
+nombreB = deuxiemeNombre;
+
+function addition(nombreA, nombreB) {
+    
+    result = nombreA + nombreB;
+
+    return result;
+
+}
+
+
+
+
+
