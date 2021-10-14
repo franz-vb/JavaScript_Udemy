c25dd879fffc3bc68f8e66104008c0ec741dc8eb
franz von büren <fz.v.bueren@gmail.com>
2021-10-14 12:27:30 +0200

    add 11

---
+++ 11_fonctions_recursives/exemple.js
@@ -0,0 +1,25 @@
+function somme(nombre) { // 3
+    
+    if(nombre == 1) {
+        return 1;
+    }
+
+    return nombre + somme(nombre - 1); // 2
+
+        // retourner 2 + somme(1)
+
+            // retourner 1
+
+    // 3 + 2 + 1
+}
+
+console.log(somme(3));
+
+
+
+
+
+
+
+
+
--- exemple.js
+++ exemple.js
@@ -1,4 +1,12 @@
+function somme(nombre) { // 5
+ 
+    if(nombre ==1){
+        return 1;
+    }
 
+}
+
+console.log(somme(5));
 
 
 
