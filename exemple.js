e9c286aefbe6fab23163746177bd0d64e07ad7bc
franz von büren <fz.v.bueren@gmail.com>
2021-11-16 11:00:28 +0100

    add tuesday

--- exemple.js
+++ exemple.js
@@ -1,24 +1,37 @@
-// Page index.
+// Page index/accueil.
 
-// - Je commence par récupérer l'ensemble des produits à l'aide de l'API avec fetch.
-// - Je récupere les informations/éléments qui me sont utile pour la page index (l'ID, l'image, la description image, le nom, la description).
+// - Avec fetch je récupère l'ensemble des produits (je rècupère la réponse émise).
+// - Ensuite je la parcoure (avec une boucle), pour inserer chaque produit dans la page d'accueil (l'ID, l'image, la description image, le nom, la description),
+//   à l'aide d'un querySelector.innerHTML. 
 
-// De la page index à la page produit.
+// Page produit.
 
-// - Je dois récupérer l'ID du produit de la page index/d'acceuil (cliqué), qui se trouve dans l'URL de la nouvelle page produit.
-// - Une fois récupéré, je fetch pour récupérer les différentes information de ce produit, que j'insere dans la page produit.
+// - Je dois récupérer l'ID du produit de la page index/d'accueil (qui as été cliqué), qui se trouve dans l'URL de la nouvelle page produit (à l'aide de méthode
+//   URLSearchParams).
+// - Je fetch pour récupérer les différentes information de ce produit, que j'insere dans la page produit. En utilisant des textContent et des innerHTML,
+//   je dois utiliser une (boucle) pour retrouver la couleur sélectionné.
 // - Maintenant je dois avoir la possibilité d'ajouter ce produit au panier. en mettant le produit avec ses différentes caratéristiques
 //   dans le localStorage.
-// - Quand on ajoute un produit au panier: si il n'était pas présent on ajoute une nouvelle ligne au panier. Si il était déja présent (avec
-//   avec la même couleur et ID), on incrémente la quantité.
+// - Quand je click sur le bouton "ajouter un produit", 
+// - Lorsqu’on ajoute un produit au panier, (si) celui-ci n'était pas déjà présent dans le panier, on ajoute un nouvel élément.
+// - Lorsqu’on ajoute un produit au panier, si celui-ci était déjà présent
+//   dans le panier (même id + même couleur), on incrémente
+//   simplement la quantité du produit.
 
-// De la page produit à la page panier.
+// Page panier.
 
-// - Depuis la page panier je récupère les infos du localStorage.
-// - On doit voir les différentes lignes de produit (même couleur + même id).
+// - Depuis la page Panier, récupérer le panier  via localStorage.
+// - bouclé le panier
+// - Créer et insérer des éléments dans la page Panier.
 // - On doit avoir la possibilité de modifier et supprimer un produit.
 // - Je dois récupérer les données saisi par l'utilisateur, et etre sur que le formulaire est remplie correctement.
+// - Constituer un objet contact (à partir des données du formulaire) et un tableau de produits.
 
-// De la page panier à la commande.
+// Afficher numéro de commande.
 
-// - Ensuite je dois afficher un numéro de commande. et rediriger l'utilisateur sur la page commande.
+// - Effectuer une requête POST sur l’API (avec fetch) et récupérer l’identifiant de commande dans la réponse de celle-ci.
+// - Rediriger l’utilisateur sur la page Confirmation, en passant l’id de commande dans l’URL, dans le but d’afficher le numéro de commande.
+
+
+
+
--- fetch/exemple.js
+++ fetch/exemple.js
@@ -113,3 +113,83 @@ btnPanier.addEventListener("click", () => {//"addEventListener" méthode qui éc
 })
 
 getProductFromId();
+:::::::::::::::::::::::::::::::::::
+const urlParams = new URLSearchParams(window.location.search); 
+const idProduct = urlParams.get("id");
+const btnCart = document.querySelector("#addToCart");
+const itemQty = document.querySelector('#quantity');
+                
+let productTitle = document.querySelector("#title");
+let productDescription = document.querySelector("#description");
+let productPrice = document.querySelector("#price");
+let productImg = document.querySelector(".item__img");
+let productColorsOptions = document.querySelector('#colors');
+let quantity = document.querySelector("#quantity");
+let cart ;
+
+function getProductFromId() {
+  fetch("http://localhost:3000/api/products/" + idProduct )
+  .then(response => response.json())
+  .then(product => { 
+
+      productTitle.textContent = `${product.name}`;
+      productDescription.textContent = `${product.description}`
+      productPrice.textContent = `${product.price}`;
+      productImg.innerHTML += `<img id=productImg src=${product.imageUrl} alt=Photographie d'un canapé />`;
+
+      product.colors.forEach((color) => {
+          productColorsOptions.innerHTML += `<option value="${color}">${color}</option>`;
+      })
+  });
+};
+
+getProductFromId();
+               
+btnCart.addEventListener("click", (e) => {
+  
+  if(localStorage.getItem("panier")) {
+    cart = JSON.parse(localStorage.getItem("panier"));
+    console.log(cart);
+  } else{
+    cart = [];
+  }
+  
+  cart.push({id: idProduct, quantity: parseInt(quantity.value), color: productColorsOptions.value, name: productTitle.textContent, price: productPrice.textContent, img: document.querySelector("#productImg").src});
+  localStorage.setItem("panier", JSON.stringify(cart));
+  
+});
+.......................
+let cart = JSON.parse(localStorage.getItem("panier"));
+//console.log(cart);
+
+function getProductFromCart() {
+    
+            cart.forEach((product) => {
+                console.log(product)
+                document.querySelector("#cart__items").innerHTML += `
+                    <article class="cart__item" data-id="{product-ID}">
+                        <div class="cart__item__img">
+                        <img src="${product.img}" alt="Photographie d'un canapé">
+                        </div>
+                        <div class="cart__item__content">
+                        <div class="cart__item__content__titlePrice">
+                            <h2>${product.name}</h2>
+                            <p>42,00 €</p>
+                        </div>
+                        <div class="cart__item__content__settings">
+                            <div class="cart__item__content__settings__quantity">
+                            <p>Qté : </p>
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                            </div>
+                            <div class="cart__item__content__settings__delete">
+                            <p class="deleteItem">Supprimer</p>
+                            </div>
+                        </div>
+                        </div>
+                    </article>`;
+            })
+            
+        };
+
+getProductFromCart();
+//console.log(cart);
